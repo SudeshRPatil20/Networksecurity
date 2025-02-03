074775adefb6d0988dfd9f5c6d2cff7102b7fa32
SudeshRPatil20 <Sudeshrpatil201@gmail.com>
2025-02-03 20:57:35 +0530

    completed model training and mlflow

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -18,7 +18,8 @@ TRAIN_FILE_NAME : str="train.csv"
 TEST_FILE_NAME: str="test.csv"
 
 SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
-
+SAVED_MODEL_DIR=os.path.join("saved_models")
+MODEL_FILE_NAME= "model.pkl"
 
 """ 
 data ingesion releted constant starts with Data_ingetion_var_name
@@ -59,3 +60,18 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict ={
     "n_neighbors": 3,
     "weights" : "uniform",
 }
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+
+""" 
+model trainer related constant starts with model trainer var name
+"""
+
+MODEL_TRAINER_DIR_NAME: str ="model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str ="trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str ="model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
